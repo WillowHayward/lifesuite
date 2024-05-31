8fe82094d4b2996548dc3e0060e69a9d294f062a
Willow Hayward <willow@whc.fyi>
2024-05-31 23:21:30 +1000

    feat: Implemented basic log parsing & readme

---
+++ README.md
@@ -0,0 +1,28 @@
+# Horace - A Command Line Life Journalling Tool
+## This Is Your Life
+
+Horace is a command line utility to help you keep track of things in your life. It is inspired by [Task Warrior](#) and [Nomie](#).
+
+
+# Usage
+## Don't Be Afraid - Just Knock On The Door
+
+The command to use Horace is `life`. When no command is specified, it will search for logs
+
+## log
+
+The heart of Horace - `life log <whatever>` will add a new log entry, dated at the time the command is entered, containing whatever you put in `<whatever>`.
+
+# Concepts
+
+## Log
+
+A log is a journal entry.
+
+## Tag
+
+A tag is a label for a log that can optionally have a value attached to it. The syntax for this is either `+metric`, `+metric:value`, or `+metric:value:value:...:value` where `metric` can be any alphanumeric string of text, and `value` is whatever value (text or number) you can to store for that metric in the particular log. A tag can have any number of values attached to it. Values are stored positionally - `+tag:1:2:3` will always have a first value 1, a second value of 2, and a third value of 3.
+
+## Person
+
+A person is someone who might be mentioned in a log. The syntax for this is `@name`, `@organisation.name`, or `@organisation.suborganisation.name` where `name`, `organisation`, and `suborganisation` can be any alphanumeric string of text. You can have as many suborganisations as you please.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,88 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        let search = Vec::new();
+        display_logs(&search);
+        return;
+    }
+
+    match args[1].as_str() {
+        "log" => build_log(&args[2..]),
+        _ => display_logs(&args[1..]),
+    }
+
+    /*let log = HashMap::new();
+    let tags =
+    for arg in args {
+        println!("{}", arg);
+    }*/
+}
+
+fn display_logs(search: &[String]) {
+    println!("Displaying logs relating to '{}'", search.join(" "));
+}
+
+struct Log {
+    text: String,
+    tags: Vec<Tag>,
+    people: Vec<Person>,
+}
+
+fn build_log(log: &[String]) {
+    let mut tags: Vec<Tag> = Vec::new();
+    let mut people: Vec<Person> = Vec::new();
+    for part in log {
+        if part.starts_with("+") {
+            let tag = parse_tag(&part[1..]);
+            tags.push(tag);
+        } else if part.starts_with("@") {
+            let person = parse_person(&part[1..]);
+            people.push(person);
+        }
+    }
+
+    let log = Log {
+        text: log.join(" "),
+        tags,
+        people,
+    };
+
+    println!("Log: {}", log.text);
+    for tag in log.tags {
+        println!("    Tag: {}", tag.name);
+        for value in tag.values {
+            println!("        Value: {}", value);
+        }
+    }
+    for person in log.people {
+        println!("\tPerson: {}", person.name);
+    }
+}
+
+struct Tag {
+    name: String,
+    values: Vec<String>,
+}
+
+fn parse_tag(full_tag: &str) -> Tag {
+    let parts: Vec<String> = full_tag.split(":").map(|s| s.to_string()).collect();
+    let tag = &parts[0];
+    let values = &parts[1..];
+    return Tag {
+        name: tag.to_string(),
+        values: values.to_vec(),
+    };
+}
+
+struct Person {
+    name: String,
+}
+
+fn parse_person(person: &str) -> Person {
+    return Person {
+        name: person.to_string(),
+    };
 }
